8e7a1af022907708ebdbb08b4aca7bc8297762d6
Yaser Basravi <basraviyaserm@gmail.com>
2025-01-05 12:19:17 +0530

    added jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -27,10 +30,16 @@
                 </header>
             @endif
 
+            <!-- Notification Component -->
+            <x-notification />
+
             <!-- Page Content -->
             <main>
-                @yield('content')
+                {{ $slot }}
             </main>
         </div>
+
+        <!-- Stack Scripts -->
+        @stack('scripts')
     </body>
 </html>
