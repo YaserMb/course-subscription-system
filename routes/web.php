fa8d6ed8ecabec432339b8d7a8dbda45e00ff575
Yaser Basravi <basraviyaserm@gmail.com>
2025-01-05 12:16:24 +0530

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,25 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubscriptionPlanController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', 'check.subscription'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified', 'check.subscription'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('subscription-plans/plans',[SubscriptionPlanController::class, 'plans'])->name('subscription-plans.plans');
+    Route::post('subscription-plans/{subscriptionPlan}/subscribe', [SubscriptionPlanController::class, 'subscribe'])->name('subscription-plans.subscribe');
+    Route::post('/courses/{course}/download', [CourseController::class, 'download'])->name('courses.download');
 });
 
 Route::resource('subscription-plans', SubscriptionPlanController::class);
-
-Route::post('subscription-plans/{subscriptionPlan}/subscribe', [SubscriptionPlanController::class, 'subscribe'])
-    ->name('subscription-plans.subscribe');
-
-
 
 require __DIR__.'/auth.php';
