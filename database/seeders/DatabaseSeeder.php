478fe850008409cd7523ff718a4e80949e0b8b78
Yaser Basravi <basraviyaserm@gmail.com>
2025-01-05 17:53:21 +0530

    removed course seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             SubscriptionPlanSeeder::class,
-            CourseSeeder::class,
         ]);
     }
 }
