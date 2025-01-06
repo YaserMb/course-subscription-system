6ab90c794a142bd799e55b0226cdd4d2f6dae4be
Yaser Basravi <basraviyaserm@gmail.com>
2025-01-06 12:49:06 +0530

    removed middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\CheckSubscription;
 use App\Http\Middleware\VerifyDownloadToken;
 use App\Http\Middleware\CheckActiveSubscription;
 
@@ -16,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'check.subscription' => CheckSubscription::class,
             'admin' => AdminMiddleware::class,
             'verify.download' => VerifyDownloadToken::class,
             'active.subscription' => CheckActiveSubscription::class,
